f79dafacf55bcafca94203df4ec98f2dc0ea71c3
Irsacfac <irsacfac@gmail.com>
2021-10-06 19:08:41 -0600

    Update tablas.sql

---
+++ Base de datos/tablas.sql
@@ -0,0 +1,76 @@
+CREATE TABLE TipoUsuario(
+	  [Id]					INT NOT NULL IDENTITY(1,1),
+	  [Nombre]				VARCHAR(32) NOT NULL,
+	  PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
+CREATE TABLE Unidad(
+	  [Id]					INT NOT NULL IDENTITY(1,1),
+	  [Nombre]				VARCHAR(32) NOT NULL,
+	  PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
+CREATE TABLE Etiqueta(
+	  [Id]					INT NOT NULL IDENTITY(1,1),
+	  [Nombre]				VARCHAR(32) NOT NULL,
+	  PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
+CREATE TABLE Usuario(
+	  [Id]					INT NOT NULL IDENTITY(1,1),
+	  [IdTipoUsuario]		INT NOT NULL,
+	  [Nombre]				VARCHAR(32) NOT NULL,
+	  [Telefono]			VARCHAR(16) NOT NULL,
+	  [Email]				VARCHAR(32) NOT NULL,
+	  [Contrasenia]			VARCHAR(32) NOT NULL,
+	  [Activo]				BIT NOT NULL,
+	  PRIMARY KEY CLUSTERED ([ID] ASC),
+	  FOREIGN KEY ([IdTipoUsuario]) REFERENCES [dbo].[TipoUsuario] ([ID])
+);
+
+CREATE TABLE Biomasa(
+	  [Id]					INT NOT NULL IDENTITY(1,1),
+	  [IdUsuario]			INT NOT NULL,
+	  [IdUnidad]			INT NOT NULL,
+	  [Nombre]				VARCHAR(32) NOT NULL,
+	  [Descripcion]			VARCHAR(256) NOT NULL,
+	  [Precio]				MONEY NOT NULL,
+	  [Cantidad]			INT NOT NULL,
+	  [Activo]				BIT NOT NULL,
+	  PRIMARY KEY CLUSTERED ([ID] ASC),
+	  FOREIGN KEY ([IdUsuario]) REFERENCES [dbo].[Usuario] ([ID]),
+	  FOREIGN KEY ([IdUnidad]) REFERENCES [dbo].[Unidad] ([ID])
+);
+
+CREATE TABLE BiomasaXEtiqueta(
+	  [Id]					INT NOT NULL IDENTITY(1,1),
+	  [IdBiomasa]			INT NOT NULL,
+	  [IdEtiqueta]			INT NOT NULL,
+	  PRIMARY KEY CLUSTERED ([ID] ASC),
+	  FOREIGN KEY ([IdBiomasa]) REFERENCES [dbo].[Biomasa] ([ID]),
+	  FOREIGN KEY ([IdEtiqueta]) REFERENCES [dbo].[Etiqueta] ([ID])
+);
+
+--DELETE FROM [dbo].[TipoUsuario];
+--DBCC CHECKIDENT([TipoUsuario], RESEED, 0)
+--SELECT * FROM [dbo].[TipoUsuario]
+
+--DELETE FROM [dbo].[Unidad];
+--DBCC CHECKIDENT([Unidad], RESEED, 0)
+--SELECT * FROM [dbo].[Unidad]
+
+--DELETE FROM [dbo].[Etiqueta];
+--DBCC CHECKIDENT([Etiqueta], RESEED, 0)
+--SELECT * FROM [dbo].[Etiqueta]
+
+--DELETE FROM [dbo].[Usuario];
+--DBCC CHECKIDENT([Usuario], RESEED, 0)
+--SELECT * FROM [dbo].[Usuario]
+
+--DELETE FROM [dbo].[Biomasa];
+--DBCC CHECKIDENT([Biomasa], RESEED, 0)
+--SELECT * FROM [dbo].[Biomasa]
+
+--DELETE FROM [dbo].[BiomasaXEtiqueta];
+--DBCC CHECKIDENT([BiomasaXEtiqueta], RESEED, 0)
+--SELECT * FROM [dbo].[BiomasaXEtiqueta]
