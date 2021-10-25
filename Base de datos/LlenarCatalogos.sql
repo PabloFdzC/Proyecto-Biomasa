788e1461d7a5ff762c58d0e929530711606fcfd3
Irsacfac <irsacfac@gmail.com>
2021-10-24 20:23:56 -0600

    Update LlenarCatalogos.sql

--- Base de datos/LlenarCatalogos.sql
+++ Base de datos/LlenarCatalogos.sql
@@ -1,11 +1,17 @@
---EXEC [dbo].[CreateTipoUsuario] 'Administrador'
---EXEC [dbo].[CreateTipoUsuario] 'Comprador'
---EXEC [dbo].[CreateTipoUsuario] 'Vendedor'
-
---SELECT * FROM [dbo].[TipoUsuario]
+EXEC [dbo].[CreateTipoUsuario] 'Administrador'
+EXEC [dbo].[CreateTipoUsuario] 'Comprador'
+EXEC [dbo].[CreateTipoUsuario] 'Vendedor'
 
 EXEC [dbo].[CreateUnidad] 'Toneladas'
+EXEC [dbo].[CreateUnidad] 'Kilogramos'
+EXEC [dbo].[CreateUnidad] 'Metros cubicos'
 
 EXEC [dbo].[CreateEtiqueta] 'Liquido'
 EXEC [dbo].[CreateEtiqueta] 'Residuos'
 EXEC [dbo].[CreateEtiqueta] 'Solido'
+EXEC [dbo].[CreateEtiqueta] 'Humedo'
+EXEC [dbo].[CreateEtiqueta] 'Seco'
+
+SELECT * FROM [dbo].[TipoUsuario]
+SELECT * FROM [dbo].[Unidad]
+SELECT * FROM [dbo].[Etiqueta]
